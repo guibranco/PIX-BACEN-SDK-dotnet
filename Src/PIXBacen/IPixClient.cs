a9df1f137fb101e2775938d0ca6311ce863bbf41
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-28 14:17:55 +0100

    Add SonarCloud integration workflow for Pixeebot (#121)
    * Create sonarcloud-pixeebot.yml
    
    * CSharpier format
    
    ---------
    
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- Src/PIXBacen/IPixClient.cs
+++ Src/PIXBacen/IPixClient.cs
@@ -6,18 +6,43 @@
 
     public interface IPixClient
     {
-        ValueTask<string> GetChargePayloadAsync(string accessToken, CancellationToken cancellationToken);
+        ValueTask<string> GetChargePayloadAsync(
+            string accessToken,
+            CancellationToken cancellationToken
+        );
 
-        ValueTask<string> GetChargePayloadWithDueDateAsync(string accessToken, string ibgeCode, DateTime desiredPaymentDate, CancellationToken cancellationToken);
+        ValueTask<string> GetChargePayloadWithDueDateAsync(
+            string accessToken,
+            string ibgeCode,
+            DateTime desiredPaymentDate,
+            CancellationToken cancellationToken
+        );
 
-        ValueTask<CobrancaPayload> PutChargeAsync(string txId, CobrancaPayload charge, CancellationToken cancellationToken);
+        ValueTask<CobrancaPayload> PutChargeAsync(
+            string txId,
+            CobrancaPayload charge,
+            CancellationToken cancellationToken
+        );
 
-        ValueTask<CobrancaPayload> PatchChargeAsync(string txId, CobrancaPayload charge, CancellationToken cancellationToken);
+        ValueTask<CobrancaPayload> PatchChargeAsync(
+            string txId,
+            CobrancaPayload charge,
+            CancellationToken cancellationToken
+        );
 
-        ValueTask<CobrancaPayload> GetChargeAsync(string txtId, CancellationToken cancellationToken);
+        ValueTask<CobrancaPayload> GetChargeAsync(
+            string txtId,
+            CancellationToken cancellationToken
+        );
 
-        ValueTask<CobrancaPayload> PostChargeAsync(CobrancaPayload charge, CancellationToken cancellationToken);
+        ValueTask<CobrancaPayload> PostChargeAsync(
+            CobrancaPayload charge,
+            CancellationToken cancellationToken
+        );
 
-        ValueTask<CobrancaPayloadList> GetChargesAsync(ChargeFilter filters, CancellationToken cancellationToken);
+        ValueTask<CobrancaPayloadList> GetChargesAsync(
+            ChargeFilter filters,
+            CancellationToken cancellationToken
+        );
     }
 }
